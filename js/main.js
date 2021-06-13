74a14be521ae30bba2af6a6136257e8f84c89cb8
Pipuss <45914032+Pipuss@users.noreply.github.com>
2021-06-13 11:31:12 +0300

    Финал

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
-/* eslint-disable no-console */
-
-// Описываем функцию, которая проверяет совпадает ли диапазон с условием задачи и дает допуск искать из диапазона рандомное число в следующих функциях.
-const isAdmission = (min, max) => (min >= 0 && max >= 0 && min < max);
-
-// Далее описываем функции для возвражения случайных чисел из диапазона
 const getRandomInteger = (rangeMin, rangeMax) => {
-  if (isAdmission(rangeMin, rangeMax)) {
-    return console.log(`Целое число из диапазона: ${Math.floor(rangeMin + Math.random() * (rangeMax + 1 - rangeMin))}`);
+  if (rangeMin % 1 !== 0 && rangeMax !== 0) {
+    throw new Error('В качестве аргументов могут использоваться только целые числа');
   }
-  console.log('Что то пошло не так. Проверь положительные ли числа и не равны ли друг другу.');
+
+  const minValue = Math.min(rangeMin, rangeMax);
+  const maxValue = Math.max(rangeMin, rangeMax);
+
+  return Math.floor(minValue + Math.random() * (maxValue - minValue));
 };
 
 const getRandomFloat = (rangeMin, rangeMax, floatNumber) => {
-  if (isAdmission(rangeMin, rangeMax)) {
-    return console.log(`Число с плавающей точкой из диапазона: ${(rangeMin + Math.random() * (rangeMax + 1 - rangeMin)).toFixed(floatNumber)}`);
-  }
-  console.log('Что то пошло не так. Проверь положительные ли числа и не равны ли друг другу.');
+  const minValue = Math.min(rangeMin, rangeMax);
+  const maxValue = Math.max(rangeMin, rangeMax);
+  return (minValue + Math.random() * (maxValue + 1 - maxValue)).toFixed(floatNumber);
 };
 
-getRandomInteger(1, 10);
-getRandomFloat(35.65000, 35.70000, 5);
+getRandomInteger();
+getRandomFloat();
