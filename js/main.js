e7a74b247a5e0201b67a9b831602cd6cc1549fc1
fuckhtml <jordenblacksnk@gmail.com>
2021-07-20 21:40:05 +0400

    review

--- js/main.js
+++ js/main.js
@@ -1,7 +1,50 @@
-import{numberOfOffers} from './variables.js';
-import{getRandomInteger} from './utils.js';
-import{similarOffers} from './generate-template-data.js';
-import{createSimilarElement} from './generate-similar-offers.js';
+const showError = (err) => {
+  alert(err);
+}
 
-createSimilarElement(similarOffers[getRandomInteger(0, numberOfOffers)]);
+const renderMarkers = (data) => {
+  console.log(data);
+}
+
+// fetch(`https://23.javascript.pages.academy/keksobooking/data`)
+//   .then(response => response.json())
+//   .then(data => renderMarkers(data))
+//   .catch(err => showError(err))
+
+const deactivateApp = () => {
+  console.log(`App is deactivated`);
+}
+
+const activateApp = () => {
+  console.log(`App is activated`);
+}
+
+const initMap = () => {}
+
+
+
+
+
+// Неактивное состояние (заблокирована карта фильтры форма)   <==========
+// *Инициализировать карту. Then:   <========
+//   Перевести в активное состояние:
+//    Скачать данные с сервера. Then:
+//      Отрисовать маркеры
+//      Установить фильтры
+//      Установить форму            <========
+
+
+
+
+
+
+deactivateApp();
+
+// initMap()
+//   .then(() => activateApp())
+//   .catch((err) => showError(err))
+
+
+
+
 
