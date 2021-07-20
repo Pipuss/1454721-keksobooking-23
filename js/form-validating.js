faa3dd86dcdc230b9dc0b748bc82cabb7dae84cc
Pipuss <45914032+Pipuss@users.noreply.github.com>
2021-07-20 11:26:50 +0300

    Прими эту домашку на глаз. Суть ли нормально сделана. Модульность и рефакторинг сделаю в следующей. Отправляю как есть, ибо нужно ждать пока ты проверишь, а я буду двигаться дальше, потому что времени хватает, но очень мало

---
+++ js/form-validating.js
@@ -0,0 +1,79 @@
+const addOfferForm = document.querySelector('.ad-form');
+const offerTitle = addOfferForm.querySelector('#title');
+const offerPrice = addOfferForm.querySelector('#price');
+const offerRoomsNumber = addOfferForm.querySelector('#room_number');
+const offerRoomsCapacity = addOfferForm.querySelector('#capacity');
+const offerRoomsType = addOfferForm.querySelector('#type');
+const offerTimeIn = addOfferForm.querySelector('#timein');
+const offerTimeOut = addOfferForm.querySelector('#timeout');
+
+const offerPriceDepending = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
+
+offerTitle.addEventListener('input', () => {
+  const minLength = offerTitle.minLength;
+  const maxLength = offerTitle.maxLength;
+
+  const valueLength = offerTitle.value.length;
+
+  if (valueLength < minLength) {
+    offerTitle.setCustomValidity(`Не хватает ещё ${minLength - valueLength} символов`);
+  } else if (valueLength > maxLength) {
+    offerTitle.setCustomValidity(`Длиннее возможного на ${valueLength - maxLength} символов`);
+  } else {
+    offerTitle.setCustomValidity('');
+  }
+
+  offerTitle.reportValidity();
+});
+
+const changeOfferMinPrice = (data, optionElement, priceElement) => {
+  priceElement.placeholder = data[optionElement.querySelector(':checked').value];
+};
+
+offerRoomsType.addEventListener('change', () => {
+  changeOfferMinPrice(offerPriceDepending, offerRoomsType, offerPrice);
+});
+
+offerPrice.addEventListener('input', () => {
+  const maxPrice = +offerPrice.max;
+  const minPrice = +offerPrice.min;
+  const price = +offerPrice.value;
+
+  if (price > maxPrice) {
+    offerPrice.setCustomValidity(`Укажите цену не больше ${maxPrice}`);
+  } else if (price < minPrice) {
+    offerPrice.setCustomValidity(`Укажите цену не меньше ${minPrice}`);
+  } else {
+    offerPrice.setCustomValidity('');
+  }
+
+  offerPrice.reportValidity();
+});
+
+offerTimeIn.addEventListener('change', () => {
+  const timesIn = offerTimeIn.querySelectorAll('option');
+  const timesOut = offerTimeOut.querySelectorAll('option');
+  // eslint-disable-next-line id-length
+  timesIn.forEach((timeIn, i) => {
+    if (timeIn.selected) {
+      timesOut[i].selected = 'selected';
+    }
+  });
+});
+
+offerRoomsNumber.addEventListener('change', () => {
+  const roomNumbers = offerRoomsNumber.querySelectorAll('option');
+  const roomCapacities = offerRoomsCapacity.querySelectorAll('option');
+  // eslint-disable-next-line id-length
+  roomNumbers.forEach((roomNumber, i) => {
+    if (roomNumber.selected) {
+      roomCapacities[i].selected = 'selected';
+    }
+  });
+});
--- js/main.js
+++ js/main.js
@@ -4,3 +4,4 @@ import{similarOffers} from './generate-template-data.js';
 import{createSimilarElement} from './generate-similar-offers.js';
 
 createSimilarElement(similarOffers[getRandomInteger(0, numberOfOffers)]);
+
